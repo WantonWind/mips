e36c7a24941f804239f3c52277931c97ff888cf8
WantonWind <875357936@qq.com>
2017-06-29 17:37:35 +0800

    6/29 PM

--- mips_first.cpp
+++ mips_first.cpp
@@ -10,6 +10,9 @@ const int SHF = 0x0000ffff;
 const int CHF = 0x000000ff;
 const int lo = 32;
 const int hi = 33;
+const int v0 = 2;
+const int a0 = 4;
+const int a1 = 5;
 typedef long long ll;
 
 using namespace std;
@@ -153,13 +156,13 @@ int get_string(int a, int b){
 //class ins used to store instruction briefly
 struct ins{
 	int op;		//操作类型+功能
-	int rs;		//第一个操作数的寄存器地址
+	int rs;		//第一个操作数的寄存器地址 (不存在为-1 load/store)
 	int rt;		//第二个操作数的寄存器地址（当第二个数不存在或者为立即数的时候默认为-1）
 	int rd;		//结果寄存器地址（-1代表low和high）
 	int sc;		//位移量/立即数   //??? why use ll
 	int lp;		//label 序号
 
-	ins(){}
+	ins(){}		//rs rs -1??
 	ins(string ex){
 		// to do
 	}
@@ -172,7 +175,7 @@ protected:
 	ins ii;
 	int clock;
 public:
-	regulator(ins ex):ii(ex),clock(1){}
+	regulator(ins ex):ii(ex),clock(2){}
 	int stage(){ return clock; }
 	virtual int console() = 0;
 	virtual bool is_controlor() = 0;
@@ -191,6 +194,11 @@ private:
 			A = reg[ii.rs];
 			if(ii.rt == -1) B = ii.sc;
 			else B = reg[ii.rt];
+			if(ii.rd != -1) reg_occupied[ii.rd] = true;
+			else{
+				reg_occupied[lo] = true;
+				reg_occupied[hi] = true;
+			}
 			return true;
 		}
 		else return false;
@@ -205,8 +213,13 @@ private:
 		if(ii.rd == -1){
 			reg[lo] = ans.first;
 			reg[hi] = ans.second;
+			reg_occupied[lo] = false;
+			reg_occupied[hi] = false;
 		}
-		else reg[ii.rd] = ans.first;
+		else{
+			reg[ii.rd] = ans.first;
+			reg_occupied[ii.rd] = false;
+		}
 	}
 public:
 	calculator(const ins& ex):regulator(ex){}
@@ -271,32 +284,205 @@ public:
 
 class load : public regulator{
 private:
+	int pa;		//original address / mem_buffer
 	int ans;
 	bool prepare(){
-		if()
+		if(ii.rs == -1){
+			reg_occupied[ii.rd] = true;
+			pa = ii.lp;
+			return true;
+		}
+		if(!reg_occupied[ii.rs]){
+			reg_occupied[ii.rd] = true;
+			pa = reg[ii.rs];
+			return true;
+		}
+		return false;
+	}
+	
+	void execute(){
+		if(ii.rs == -1){
+			ans = pa;
+		}
+		else ans = pa + ii.sc;
+	}
+	
+	void mem_access(){
+		pa = 0;
+		if(ii.op == 42) pa = ans;
+		if(ii.op == 43) pa += data[ans];
+		if(ii.op == 44) for(int i = 0; i < 2; ++i)	pa += int(data[ans+i]) << (8 - i * 8);
+		if(ii.op == 45) for(int i = 0; i < 4; ++i)	pa += int(data[ans+i]) << (24 - i * 8);
+	}
+	
+	void write_back(){
+		reg[ii.rd] = pa;
+		reg_occupied[ii.rd] = false;
 	}
 public:
-	load(const ins& ex):regualtor(ex){}
+	load(const ins& ex):regulator(ex){}
 	int console(){
-		
+		int suc = true;
+		if(stage() == 2) suc = prepare();
+		if(stage() == 3) execute();
+		if(stage() == 4) mem_access();
+		if(stage() == 5) write_back();
+		if(suc) ++clock;
+		return suc;
 	}
 	bool is_controlor(){ return false; }
 };
 
 class store : public regulator{
+private:
+	int pa;		//original address / mem_buffer
+	int C;		//store data
+	int ans;
+	bool prepare(){
+		if(!reg_occupied[ii.rd]){
+			C = reg[ii.rd];
+			if(ii.rs == -1){
+				pa = ii.lp;
+				return true;
+			}
+			if(!reg_occupied[ii.rs]){
+				pa = reg[ii.rs];
+				return true;
+			}			
+		}
+		return false;
+	}
 	
+	void execute(){
+		if(ii.rs == -1){
+			ans = pa;
+		}
+		else ans = pa + ii.sc;
+	}
+	
+	void mem_access(){
+		if(ii.op == 46)	data[ans] = C & CHF;
+		if(ii.op == 47)	for(int i = 0; i < 2; ++i) data[ans + i] = ( C >> (8 - i * 8) ) & CHF;
+		if(ii.op == 48)	for(int i = 0; i < 4; ++i) data[ans + i] = ( C >> (24 - i * 8) ) & CHF;		
+	}
+public:
+	store(const ins& ex):regulator(ex){}
+	int console(){
+		int suc = true;
+		if(stage() == 2) suc = prepare();
+		if(stage() == 3) execute();
+		if(stage() == 4) mem_access();
+		if(suc) ++clock;
+		return suc;
+	}
+	bool is_controlor(){ return false; }	
 };
 
 class mover : public regulator{
+private:
+	int A;
+	bool prepare(){
+		if(ii.rs == -1){
+			reg_occupied[ii.rd] = true;
+			A = ii.sc;
+			return true;
+		}
+		if(!reg_occupied[ii.rs]){
+			reg_occupied[ii.rd] = true;
+			A = reg[ii.rs];
+			return true;
+		}
+		return false;
+	}
 	
+	void write_back(){
+		reg[ii.rd] = A;
+		reg_occupied[ii.rd] = false;
+	}
+public:
+	mover(const ins& ex):regulator(ex){}
+	int console(){
+		int suc = true;
+		if(stage() == 2) suc = prepare();
+		if(stage() == 4) suc = -1;
+		if(stage() == 5) write_back();
+		if(suc) ++clock;
+		return suc;
+	}
+	bool is_controlor(){ return false; }
 };
 
 class nop : public regulator{
-	
+public:
+	nop(const ins& ex):regulator(ex){}
+	int console(){
+		int suc = true;
+		if(stage() == 4) suc = -1;
+		++clock;
+		return suc;
+	}
+	bool is_controlor(){ return false; }
 };
 
-class syscall : public regualtor{
+class syscall : public regulator{
+private:
+	int v0_data;		//store the data of $v0
+	int a0_data;		//store the data of $a0
+	int a1_data;		//store the data of $a1
+	string s;
 	
+	bool prepare(){
+		if(!reg_occupied[a0] && !reg_occupied[a1]){
+			reg_occupied[v0] = true;
+			v0_data = reg[v0];
+			a0_data = reg[a0];
+			a1_data = reg[a1];
+			return true;
+		}
+		return false;
+	}
+	
+	void execute(){
+		switch(v0_data){
+			case 1:	
+				cout << a0_data;
+				break;
+			case 4:	
+				while(data[a0_data]) cout << data[a0_data++]; 
+				cout << '\0';
+				break;
+			case 5:	
+				cin >> v0_data;
+				break;
+			case 8:	
+				//
+				break;
+			case 9:	
+				//
+				break;
+			case 10:
+				exit(0);
+			case 17:
+				exit(a0_data);
+		}
+	}
+	
+	void write_back(){
+		reg[v0] = v0_data;
+		reg_occupied[v0] = false;
+	}
+public:
+	syscall(const ins& ex):regulator(ex){}
+	int console(){
+		int suc = true;
+		if(stage() == 2) suc = prepare();
+		if(stage() == 3) execute();
+		if(stage() == 4) ; // need polish
+		if(stage() == 5) write_back();
+		if(suc) ++clock;
+		return suc;
+	}
+	bool is_controlor(){ return true; }
 };
 
 //pipelining
@@ -309,12 +495,12 @@ private:
 	void push_regulator(const ins& ex){
 		regulator* rl;
 		if(ex.op <= 24) rl = new calculator(ex);
-		else if(ex.op <= 41) rl = new controler(ex);
+		else if(ex.op <= 41) rl = new controlor(ex);
 		else if(ex.op <= 45) rl = new load(ex);
 		else if(ex.op <= 48) rl = new store(ex);
 		else if(ex.op <= 52) rl = new mover(ex);
-		else if(ex.op == 53) rl = new nop;
-		else rl = new syscall;
+		else if(ex.op == 53) rl = new nop(ex);
+		else rl = new syscall(ex);
 		que.push_back(rl);
 	}		//process this ins
 	void pop_regulator(){
@@ -341,13 +527,13 @@ private:
 	void scan(){
 		mem_occupied = false;
 		for(deque<regulator*>::iterator it = que.begin(); it != que.end(); ++it){
-			int rec = it->console();
+			int rec = (*it)->console();
 			if(!rec) return;
-			if(it->stage() == 4 && rec != -1) mem_occupied = true;
+			if((*it)->stage() == 4 && rec != -1) mem_occupied = true;
 		}
-		if(que.begin()->stage() == 5){
+		if((*que.begin())->stage() == 6){
 			pop_regulator();
-			if(que.empty() || (--que.end()->is_control) == 0) control_hazard = false;
+			if(que.empty() || (*--que.end())->is_controlor() == 0) control_hazard = false; //*-- ??
 		}
 		ins_fecth();
 	}
